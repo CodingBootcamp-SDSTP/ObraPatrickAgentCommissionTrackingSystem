5fca7429cce0d1c417c9020e5c149b782ecd7d69
Patrick Obra <patrickobra@Patricks-MacBook-Pro.local>
2018-06-20 17:17:53 +0800

    added and modified files

--- src/AllAgentServlet.java
+++ src/AllAgentServlet.java
@@ -7,22 +7,41 @@ import java.util.ArrayList;
 
 public class AllAgentServlet extends HttpServlet
 {
-	
+	AgentCollection agents;
 
 	public void init() throws ServletException {
-		
+		AgentDataStore ads = new AgentDataStore();
+		agents = ads.getAgentCollection();
 	}
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/xml");
-		StringBuilder sb = new StringBuilder("<>");
-		sb.append("<>");
 		PrintWriter out = res.getWriter();
-		out.println(sb.toString());
-		out.close();
+		out.println(exportToXML());
+	}
+
+	public String exportToXML() {
+		StringBuilder sb = new StringBuilder("<agent>");
+		ArrayList<Agent> agentReps = agents.getAllAgents();
+		System.out.println("testing only hehe");
+		for(Agent a : agentReps) {
+			sb.append("<agent><id>");
+			sb.append(a.getId());
+			sb.append("</id><firstname>");
+			sb.append(a.getFirstName());
+			sb.append("</firstname><lastname>");
+			sb.append(a.getLastName());
+			sb.append("</lastname><age>");
+			sb.append(a.getAge());
+			sb.append("</age><email>");
+			sb.append(a.getEmail());
+			sb.append("</email></agent>");
+		}
+		sb.append("</agent>");
+		return(sb.toString());
 	}
 
 	public void destroy() {
-
+		agents = null;
 	}
 }
